f67c3afb1b8d5033eacb11cfc0bbad161b3b9ebe
rsumaiya16 <rsumaiya1602@gmail.com>
2024-12-16 03:27:12 +0600

    almostdone

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+
+let progress = 0;
 document.querySelector("#meow").addEventListener("click", () => {
     window.location.href = "meow.html";
   });
  
 
   
-  let progress = 0;
 
 
-  function updateButtons() {
+   function updateButtons() {
     document.getElementById("increaseBtn").disabled = progress >= 100;
     document.getElementById("decreaseBtn").disabled = progress <= 0;
   }
@@ -27,6 +28,26 @@ document.querySelector("#meow").addEventListener("click", () => {
     }
     updateButtons();
   }
-
+  
  
-  updateButtons();
+  function showCat() {
+    let catColor = document.getElementById("catColor").value.toLowerCase();
+    let catImageDiv = document.querySelector(".cat-image");
+    catImageDiv.innerHTML = "";
+
+    if (catColor === "black") {
+        catImageDiv.innerHTML = '<img src="blackcat.jpg" alt="Black Cat">';
+    } else if (catColor === "white") {
+        catImageDiv.innerHTML = '<img src="whitecat.jpg" alt="White Cat">';
+    } else if (catColor === "brown") {
+        catImageDiv.innerHTML = '<img src="browncat.jpg" alt="Gray Cat">';
+    } else {
+        catImageDiv.innerHTML = '<p>No matching cat color found!</p>';
+    }
+}
+function showName() {
+  let catName = document.getElementById("catName").value;
+  let writtenTextDiv = document.querySelector(".writtentext");
+
+  writtenTextDiv.innerHTML = catName ? `Hola,this is  ${catName}` : "Give me a name please 🥺";
+}
