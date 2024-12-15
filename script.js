e9531ffe1e98781e35b9e018895a17692ec465b6
rsumaiya16 <rsumaiya1602@gmail.com>
2024-12-15 17:02:57 +0600

    progressing

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 document.querySelector("#meow").addEventListener("click", () => {
     window.location.href = "meow.html";
   });
+ 
+
   
+  let progress = 0;
+
+
+  function updateButtons() {
+    document.getElementById("increaseBtn").disabled = progress >= 100;
+    document.getElementById("decreaseBtn").disabled = progress <= 0;
+  }
+  function increaseProgress() {
+    if (progress < 100) {
+      progress += 10;
+      document.getElementById("progressBar").style.width = progress + "%";
+    }
+    updateButtons(); 
+  }
+
+  
+  function decreaseProgress() {
+    if (progress > 0) {
+      progress -= 10;
+      document.getElementById("progressBar").style.width = progress + "%";
+    }
+    updateButtons();
+  }
+
+ 
+  updateButtons();
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
   content: [
     "./index.html",
+    "./meow.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
